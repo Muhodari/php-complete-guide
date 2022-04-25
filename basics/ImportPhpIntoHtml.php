2eddc782443ba7f45f380a2410f679c6f95a2f71
Muhodari <sagemuho@gmail.com>
2022-04-25 07:34:19 +0200

    added php in body of html

---
+++ basics/ImportPhpIntoHtml.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=D, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+echo "welcome to the body of the application";
+
+?>
+
+
+</body>
+</html>
+
