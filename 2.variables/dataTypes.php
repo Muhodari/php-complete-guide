cc441f72f5d4e5e94d6cb0cb0dd48e18e8489584
Muhodari <sagemuho@gmail.com>
2022-04-25 09:49:27 +0200

    variable data types

---
+++ 2.variables/dataTypes.php
@@ -0,0 +1,22 @@
+<?php
+
+#integer
+$number1= 40;
+$number2="30";
+
+
+echo $number1 + $number2; // answer is 70 php detect the data types automaticalty
+
+
+#float
+$float=454.65;
+
+#Boolean
+
+$Boolean = true;
+
+// string
+$String= "welcome back Again"
+
+
+?>
