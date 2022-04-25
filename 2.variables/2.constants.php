08fb325eed24a6ba7f6d410b56f72a34139c0be9
Muhodari <sagemuho@gmail.com>
2022-04-25 08:20:27 +0200

    variables -* constant

---
+++ 2.variables/2.constants.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+echo" <h5> Constants </h5>";
+
+#once declared it and assigned a value it can't be overwritten
+
+// $drc = "Congo My Nation";
+define("drc","Kitasha");
+#this initialization won't work since there is previous initiazation of the drc to kitasha
+define("drc","Musanze");
+
+echo drc;
+echo "<br><br>";
+echo"I love ".drc;
+echo "<br><br>";
+echo "You are Beutiful ".drc;
+echo"<br><br>";
+echo "I will cherish".drc ."<br><br>";
+
+
+
+
+?>
