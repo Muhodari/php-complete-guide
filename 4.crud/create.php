d475d3c122c78bfc9e956c9fc86ae9bed9f2d3b4
Muhodari <sagemuho@gmail.com>
2022-04-26 07:27:00 +0200

    added create data form and functionality to add data to database

---
+++ 4.crud/create.php
@@ -0,0 +1,36 @@
+<?php
+
+$name =$_POST["name"];
+$email =$_POST["email"];
+$content =$_POST["content"];
+
+
+/* echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+
+ECHO $content;
+*/
+
+
+$DB_server = "localhost";
+$DB_name = "php_study";
+$DB_user_name = "root";
+$DB_user_password = "";
+
+
+$connect = mysqli_connect($DB_server,$DB_user_name,$DB_user_password,$DB_name);
+if(!$connect){
+    echo mysqli_error();
+}
+
+$query = mysqli_query($connect,"INSERT INTO users(name,email,content) values ('$name','$email','$content')");
+
+if($query){
+    echo "<h3>User created successfully </h3>";
+    header( "refresh:2;url=form.php" );
+}
+
+
+?>
---
+++ 4.crud/form.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="create.php" method="POST">
+
+    <input type="text" name="name" placeholder="Enter Your Name" required>
+    <br>
+    <input type="email" name="email" placeholder="Enter Your Email"  required>
+    <br>
+    <input type="text" name="content"  placeholder="Enter Your content" required>
+    <br>
+                
+
+<input type="submit" value="SUBMIT">
+
+
+
+
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
